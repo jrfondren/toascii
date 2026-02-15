e0a7f2bdc8001181162758640e2bbd08781390a6
Julian Fondren <julian.fondren@gmail.com>
2026-02-15 13:11:41 -0600

    convert stdio if not given a filename, like a unix filter

--- source/app.d
+++ source/app.d
@@ -1,32 +1,43 @@
-import std.stdio: File, stderr;
-import std.format: format;
-import std.process: thisProcessID;
-import std.file: rename, remove;
-import stringex.unidecode: unidecode;
+import std.stdio : File, stderr, stdin, stdout;
+import std.format : format;
+import std.process : thisProcessID;
+import std.file : rename, remove;
+import stringex.unidecode : unidecode;
 
-bool copyLines(string inName, string outName, string function(string) convert) {
-    auto fin = File(inName, "r");
-    auto fout = File(outName, "w");
-    auto changed = false;
-    foreach (line; fin.byLineCopy) {
-        auto newLine = convert(line);
-        if (!changed && line != newLine) changed = true;
-        fout.writeln(newLine);
+bool copyLines(File fin, File fout, string function(string) convert) {
+    bool changed = false;
+    foreach (pre; fin.byLineCopy) {
+        string post = convert(pre);
+        if (!changed && pre != post)
+            changed = true;
+        fout.writeln(post);
     }
     return changed;
 }
 
 int main(string[] args) {
-    if (args.length != 2) {
-        stderr.writeln("usage: ", args[0], " <filename>");
+    switch (args.length) {
+    case 1:
+        copyLines(stdin, stdout, &unidecode);
+        break;
+    case 2:
+        bool res;
+        string tmpName = format("%s.out.%d", args[1], thisProcessID);
+        {
+            File fin = File(args[1], "r");
+            File fout = File(tmpName, "w");
+            res = copyLines(fin, fout, &unidecode);
+        }
+        if (res) {
+            tmpName.rename(args[1]);
+        } else {
+            tmpName.remove();
+            stderr.writeln("No change to input file.");
+        }
+        break;
+    default:
+        stderr.writeln("usage: ", args[0], " [<filename>]");
         return 1;
-    }
-    auto tmpName = format("%s.out.%d", args[1], thisProcessID);
-    if (copyLines(args[1], tmpName, &unidecode)) {
-        tmpName.rename(args[1]);
-    } else {
-        tmpName.remove();
-        stderr.writeln("No change to input file.");
     }
     return 0;
 }
